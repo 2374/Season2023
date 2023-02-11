370ec2874968ace63052bf216d222953362459e1
diffy971242 <diffy97124@yahoo.com>
2023-02-10 17:53:37 -0800

    Add TOF sensor to Manipulator

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -2,16 +2,20 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ManipulatorSubsystem extends SubsystemBase {
     private final WPI_TalonFX motor;
+    TimeOfFlight sensor = new TimeOfFlight(18);
 
     public ManipulatorSubsystem() {
         motor = new WPI_TalonFX(Constants.MANIPULATOR_MOTOR_PORT, Constants.CANIVORE_CAN_BUS_NAME);
         motor.setNeutralMode(NeutralMode.Brake);
+        sensor.setRangingMode(RangingMode.Short, 50);
     }
 
     public void intake() {
@@ -24,5 +28,9 @@ public class ManipulatorSubsystem extends SubsystemBase {
 
     public void stop() {
         motor.stopMotor();
+    }
+
+    public void periodic() {
+        // System.out.println("Sensor="+sensor.getRange());
     }
 }
